3fcf437a7fb77323e6af15ff691901a8b82fbfdd
ws2s <ws2s@virginia.edu>
2026-08-10 11:19:04 -0400

    Changed /Home to /home in header-global.js

--- Client/wwwroot/Home/js/components/header-global.js
+++ Client/wwwroot/Home/js/components/header-global.js
@@ -5,7 +5,7 @@ export function HeaderGlobal() {
                 <div class="flex flex-row align-items-center justify-content-between">
                     <div class="fm-logo-wrap flex justify-content-start">
                         <a href="https://www.fm.virginia.edu" class="fm-logo" aria-label="University of Virginia Facilities Management">
-                            <img src="/Home/images/layout/uva-fm-horiz-logo.svg" alt="University of Virginia Facilities Management">
+                            <img src="/home/images/layout/uva-fm-horiz-logo.svg" alt="University of Virginia Facilities Management">
                         </a>
                     </div>
                     <div class="cp-logo flex justify-content-center">
