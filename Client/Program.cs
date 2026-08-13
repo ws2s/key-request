ab0d3423f98104818e24039744b5bb82a241596a
ws2s <39894753+ws2s@users.noreply.github.com>
2026-08-13 14:31:55 +0000

    change .net from 8 to 10

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,20 +1,20 @@
 using KeyRequest;
 using KeyRequest.Client.Data.Services;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>(    
-    "#app", 
-    new RootComponentOptions
-    {
-        Parameters = new Dictionary<string, object?>
-        {
-            { "InitialMessage", "Hello from Program.cs!" },
-            { "CompID", builder.Configuration["CompID"] ?? "defaultCompID" }
-        }
-    });
+builder.RootComponents.Add<App>(
+    "#app"
+//    ,new RootComponentOptions
+//    {
+//        Parameters = new Dictionary<string, object?>
+//        {
+//            { "InitialMessage", "Hello from Program.cs!" },
+//            { "CompID", builder.Configuration["CompID"] ?? "defaultCompID" }
+//        }
+//    }
+);
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
